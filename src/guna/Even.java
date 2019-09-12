210da7523eabd0419a86fd61b982bc97352d4f24
Jayabalan9694 <jayabal9694@gmail.com>
2019-09-12 13:03:13 +0530

    even is added

---
+++ src/guna/Even.java
@@ -0,0 +1,22 @@
+package guna;
+
+public class Even {
+public static void main(String[] args) {
+		
+		for(int i=1;i<=100;i++) {
+			
+			if(i%2==0) {
+				System.out.println("i am even"+i);
+			}
+			else {
+				System.out.println(i);
+			}
+			
+			
+		}
+		
+	}
+	
+
+
+}
